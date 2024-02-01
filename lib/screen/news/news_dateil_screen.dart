900d92fa8081e74530d45e9edbc6a98bc0ae75f2
Suli244 <ayilchievsulaiman@gamil.com>
2024-02-01 22:29:27 +0600

    add

--- lib/screen/news/news_dateil_screen.dart
+++ lib/screen/news/news_dateil_screen.dart
@@ -1,5 +1,9 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:insightful_news_251/screen/news/models/news_models.dart';
+import 'package:insightful_news_251/screen/news/widget/reading_time_widget.dart';
+import 'package:insightful_news_251/utils/images/app_images.dart';
 
 class NewsDateilScreen extends StatelessWidget {
   const NewsDateilScreen({super.key, required this.model});
@@ -8,10 +12,134 @@ class NewsDateilScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-      body: Column(
-        children: [
-          Container(),
+      backgroundColor: Colors.white,
+      body: CustomScrollView(
+        physics: const ClampingScrollPhysics(),
+        slivers: [
+          SliverAppBar(
+            backgroundColor: Colors.white,
+            automaticallyImplyLeading: true,
+            expandedHeight: 415.h,
+            leading: IconButton(
+              iconSize: 24,
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: const Icon(
+                Icons.arrow_back_ios_new_outlined,
+                color: Colors.white,
+              ),
+            ),
+            actions: [
+              IconButton(
+                iconSize: 24,
+                onPressed: () {},
+                icon: Image.asset(
+                  AppImages.shareIcon,
+                  width: 24,
+                ),
+              ),
+              const SizedBox(width: 15),
+            ],
+            bottom: PreferredSize(
+              preferredSize: const Size.fromHeight(0),
+              child: Container(
+                height: 15,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(16.sp),
+                    topRight: Radius.circular(16.sp),
+                  ),
+                ),
+              ),
+            ),
+            flexibleSpace: FlexibleSpaceBar(
+              background: Stack(
+                alignment: Alignment.bottomCenter,
+                children: [
+                  CachedNetworkImage(
+                    imageUrl: model.image,
+                    width: double.maxFinite,
+                    height: double.maxFinite,
+                    fit: BoxFit.cover,
+                  ),
+                  Container(
+                    height: 327.h,
+                    width: double.infinity,
+                    decoration: const BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                          Colors.transparent,
+                          Colors.transparent,
+                          Colors.black,
+                        ],
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: 24.w,
+                    right: 24.w,
+                    bottom: 35.h,
+                    child: Column(
+                      children: [
+                        ReadingTimeWidget(
+                          categoryName: model.category,
+                          time: model.read,
+                        ),
+                        const SizedBox(height: 12),
+                        Text(
+                          model.title,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 18,
+                            fontFamily: 'SF',
+                            color: Colors.white,
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Text(
+                              model.ago,
+                              style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                fontSize: 12,
+                                fontFamily: 'SF',
+                                color: Colors.white.withOpacity(0.80),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 24),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    model.description,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      fontFamily: 'SF',
+                    ),
+                  ),
+                  SizedBox(height: 40.h),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
