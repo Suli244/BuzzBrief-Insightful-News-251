ba59eccb802bb62d25adbf4d8146088fe2718161
Эльдияр Болотов <ebolotov904@gmail.com>
2024-02-01 11:56:19 +0600

    🔥

--- lib/screen/bottom_navigation_bar/bottom_naviator_screen.dart
+++ lib/screen/bottom_navigation_bar/bottom_naviator_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:insightful_news_251/screen/famous_journalists/famous_journalists_screen.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:insightful_news_251/screen/famous_journalists/presentation/cubit/journalist_cubit.dart';
+import 'package:insightful_news_251/screen/famous_journalists/presentation/pages/journalists_page.dart';
 import 'package:insightful_news_251/screen/news/news_screen.dart';
 import 'package:insightful_news_251/screen/notifications/notifications_screen.dart';
 import 'package:insightful_news_251/screen/saved_news/saved_news_screen.dart';
@@ -18,6 +20,91 @@ class _BottomNavigatorState extends State<BottomNavigatorScreen> {
   int index = 0;
   @override
   Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => JournalistCubit(),
+      child: Scaffold(
+        body: pages[index],
+        extendBody: true,
+        bottomNavigationBar: BottomNavigationBar(
+          elevation: 0,
+          backgroundColor: Colors.white,
+          type: BottomNavigationBarType.fixed,
+          unselectedFontSize: 15,
+          selectedFontSize: 15,
+          currentIndex: index,
+          onTap: (indexFrom) async {
+            setState(() {
+              index = indexFrom;
+            });
+          },
+          items: [
+            BottomNavigationBarItem(
+              label: '',
+              icon: Image.asset(
+                AppImages.newsIcon,
+                width: 24.50,
+                color: AppColorsInsightfulNews.color00000060,
+              ),
+              activeIcon: Image.asset(
+                AppImages.newsIcon,
+                color: AppColorsInsightfulNews.color00B2FF,
+                width: 24.50,
+              ),
+            ),
+            BottomNavigationBarItem(
+              label: '',
+              icon: Image.asset(
+                AppImages.famousJournalistsIcon,
+                width: 28,
+                color: AppColorsInsightfulNews.color00000060,
+              ),
+              activeIcon: Image.asset(
+                AppImages.famousJournalistsIcon,
+                color: AppColorsInsightfulNews.color00B2FF,
+                width: 28,
+              ),
+            ),
+            BottomNavigationBarItem(
+              label: '',
+              icon: Image.asset(
+                AppImages.savedNewsIcon,
+                width: 19,
+                color: AppColorsInsightfulNews.color00000060,
+              ),
+              activeIcon: Image.asset(
+                AppImages.savedNewsIcon,
+                color: AppColorsInsightfulNews.color00B2FF,
+                width: 19,
+              ),
+            ),
+            BottomNavigationBarItem(
+              label: '',
+              icon: Image.asset(
+                AppImages.notificationsIcon,
+                width: 22,
+                color: AppColorsInsightfulNews.color00000060,
+              ),
+              activeIcon: Image.asset(
+                AppImages.notificationsIcon,
+                color: AppColorsInsightfulNews.color00B2FF,
+                width: 22,
+              ),
+            ),
+            BottomNavigationBarItem(
+              label: '',
+              icon: Image.asset(
+                AppImages.settingsIcon,
+                width: 24.50,
+                color: AppColorsInsightfulNews.color00000060,
+              ),
+              activeIcon: Image.asset(
+                AppImages.settingsIcon,
+                color: AppColorsInsightfulNews.color00B2FF,
+                width: 24.50,
+              ),
+            ),
+          ],
+        ),
     return Scaffold(
       body: pages[index],
       extendBody: true,
@@ -107,7 +194,7 @@ class _BottomNavigatorState extends State<BottomNavigatorScreen> {
 
 List<Widget> pages = [
   const NewsScreen(),
-  const FamousJournalistsScreen(),
+  const JournalistsPage(),
   const SavedNewsScreen(),
   const NotificationsScreen(),
   const SettingsScreen(),
